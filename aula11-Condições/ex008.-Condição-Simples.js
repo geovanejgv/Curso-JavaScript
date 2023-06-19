33d43793d8d3748d2c31d0a2a2fbe90aee48f492
geovanejgv <labelibijoux@gmail.com>
2023-06-18 21:53:05 -0300

    Atualização até a aula 11

---
+++ aula11-Condições/ex008.-Condição-Simples.js
@@ -0,0 +1,7 @@
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel >60){ //Condição simples
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!') //Indentação: É o recuo na programação, serve para organizar o código//
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
